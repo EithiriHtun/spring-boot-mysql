174854d02234bc030fc4af429968f935f301fc80
eithiri <eithiritun1997@gmail.com>
2020-12-18 16:10:37 +0630

    Initial commit

---
+++ src/main/java/spring/app/springframework/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package spring.app.springframework.repositories;
+
+import spring.app.springframework.domain.Product;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
